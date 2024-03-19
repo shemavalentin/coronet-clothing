a3b4d62a87428f0386a7673550cb75f584df19d4
Shema Valentin <shemavalentin@gmail.com>
2024-03-19 16:55:02 +0200

    Added decrement and encreament item quantity in checkout component

--- src/components/card-dropdown/cart-dropdown.component.js
+++ src/components/card-dropdown/cart-dropdown.component.js
@@ -1,4 +1,9 @@
 import React, { useContext } from 'react';
+
+// A react hook to allow me navigate from one page to anather
+
+import { useNavigate } from 'react-router-dom';
+
 import { CartContext } from '../../contexts/cart.contenxt';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -6,16 +11,22 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    // A function handler that calls Checkout page
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-
                 {/* Getting items into cartItem dropdown  and increase the quantity,...*/}
 
-                {cartItems.map(item => <CartItem key={ item.id } cartItem={item} />)}
-                
+                {cartItems.map(item => <CartItem key={ item.id } cartItem={item} />)}                
             </div>
-            <Button>GO TO CHECKOUT </Button>
+            <Button onClick={ goToCheckoutHandler }> GO TO CHECKOUT </Button>
         </div>
     );
 };
--- src/contexts/cart.contenxt.js
+++ src/contexts/cart.contenxt.js
@@ -8,7 +8,7 @@ const addCartItem = (cartItems, productToAdd) => {
     //find if cartItems contains productToAdd
 
     const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
-    // if found, increament wuantity
+    // if found, increament quantity
 
     if (existingCartItem) {
         return cartItems.map((cartItem) =>
@@ -21,6 +21,24 @@ const addCartItem = (cartItems, productToAdd) => {
     return [ ...cartItems, { ...productToAdd, quantity: 1}]
 }
 
+// Creating a helper function to remove cartItems from the checkout component.
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    // find the cart item to remove
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+    // check if the quantity is equal to 1, if yes, remove that item from the cart.
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+    // return back cartItems with matching cart item with reduced quantity
+
+    return cartItems.map((cartItem) =>
+            cartItem.id === cartItemToRemove.id
+                ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem )
+    }
+
 export const CartContext = createContext({
     isCartOpen: false,
     setCartIsOpen: () => { },
@@ -28,6 +46,11 @@ export const CartContext = createContext({
 
     // function to add to quantity separately and control it. 
     addItemToCart: () => { },
+    
+    // function to remove item
+
+    removeItemFromCart: () => {},
+
     cartCount: 0
 });
 
@@ -47,9 +70,14 @@ export const CartProvider = ({ children }) => {
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
+    };
+
+    // Removing the Item
+    const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
     }
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart,cartItems, cartCount };
+    const value = { isCartOpen, setIsCartOpen, addItemToCart,removeItemToCart,cartItems, cartCount };
      
     return <CartContext.Provider value={ value }>{ children }</CartContext.Provider>
 }
