5d65c81a6d3cfccc8339c667ee08c2e3d61d3ce1
Shema Valentin <shemavalentin@gmail.com>
2024-04-03 17:48:05 +0200

    Emplemented Redux Persist in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { compose, legacy_createStore, applyMiddleware } from "redux";
 //import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 // Following are step by step to write a store.
 // creating my own middleware(a currying func: a function that returns an other function )
@@ -18,6 +20,24 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
   console.log(" Next state:", store.getState);
 };
+
+// telling Redux persist what we want
+
+const persistConfig = {
+  key: "root",
+  //storage: storage
+  // or cast the key
+  storage,
+
+  // What we need to  not persist
+
+  blacklist: ["user"],
+};
+
+// Creating a persisted reducer using persistConfig
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middlewares = [loggerMiddleware];
 
 // Calling compose for middleware to work, they are like inhancers
@@ -25,7 +45,11 @@ const middlewares = [loggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
 export const store = legacy_createStore(
-  rootReducer,
+  persistedReducer,
   undefined,
   composedEnhancers
 );
+
+// Eporting persister object which calls persist store using the store object
+
+export const persistor = persistStore(store);
