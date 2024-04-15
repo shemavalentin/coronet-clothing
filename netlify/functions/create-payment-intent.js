db7a44cc3e1cca207cdf589e282d7c4485120b1c
Shema Valentin <shemavalentin@gmail.com>
2024-04-15 15:30:41 +0200

    optimized components

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,20 +1,15 @@
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-// old schooll ExpressJS function to handel payments
-
 exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
 
-    // Creating payment intent
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
       payment_method_types: ["card"],
     });
-
-    // Once payment process is done, then return an object
 
     return {
       statusCode: 200,
@@ -24,7 +19,7 @@ exports.handler = async (event) => {
     console.log({ error });
 
     return {
-      status: 400,
+      statusCode: 400,
       body: JSON.stringify({ error }),
     };
   }
