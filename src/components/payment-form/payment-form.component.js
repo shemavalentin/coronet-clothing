39f399b6d75ad4977ee59ff676f75bf919b4820c
Shema Valentin <shemavalentin@gmail.com>
2024-04-16 15:40:41 +0200

    Added spinning while processing payments, customized payments features

--- src/components/payment-form/payment-form.component.js
+++ src/components/payment-form/payment-form.component.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
 
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
-import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectCartTotal } from "../../store/cart/cart.selector";
+
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import {
+  PaymentFormContainer,
+  FormContainer,
+  PaymentButton,
+} from "./payment-form.styles";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isIsProcessingPayment, setIsProcessingPayment] = useState(false);
 
   const paymentHandler = async (e) => {
     // Pevent typical form to be submitted
@@ -15,6 +27,9 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+    // setting processing payment to true when it stats to process payments
+
+    setIsProcessingPayment(true);
 
     // Request to get the payment
     const response = await fetch("/.netlify/functions/create-payment-intent", {
@@ -24,23 +39,24 @@ const PaymentForm = () => {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
-      body: JSON.stringify({ amount: 10000 }),
+      body: JSON.stringify({ amount: amount * 100 }),
     }).then((res) => res.json());
 
     const {
       paymentIntent: { client_secret },
     } = response;
 
-    console.log(client_secret);
-
     const paymentResult = await stripe.confirmCardPayment(client_secret, {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: "Shema valentin",
+          name: currentUser ? currentUser.displayName : "Guest",
         },
       },
     });
+
+    // set processing payments to false when it ends to process
+    setIsProcessingPayment(false);
 
     if (paymentResult.error) {
       alert(paymentResult.error);
@@ -56,7 +72,12 @@ const PaymentForm = () => {
       <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
-        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now </Button>
+        <PaymentButton
+          isLoading={isIsProcessingPayment}
+          buttonType={BUTTON_TYPE_CLASSES.inverted}
+        >
+          Pay Now
+        </PaymentButton>
       </FormContainer>
     </PaymentFormContainer>
   );
