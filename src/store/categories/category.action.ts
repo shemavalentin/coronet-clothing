f2b67ec7fa1ff49c5eb9400043204753e5aac82e
Shema Valentin <shemavalentin@gmail.com>
2024-04-25 14:03:39 +0200

    Improved reducer typing

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,6 +1,6 @@
 import { CATEGORIES_ACTION_TYPES, Category} from "./category.types";
 
-import { createAction, Action, ActionWithPayload } from "../../utils/reducer/reducer.utils";
+import { createAction, Action, ActionWithPayload, withMatcher } from "../../utils/reducer/reducer.utils";
 
 // Typing FetchCategoriesStart action type
 export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>
@@ -14,19 +14,17 @@ export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FE
 // The above are the three action types our reducer will accept.
 // Let' create a Descriminatory union to be able to pass these action types to reducer.
 
-export type CategoryAction = FetchCategoriesStart | FetchCategoriesSuccess | FetchCategoriesFailed
-
-
+// export type CategoryAction = FetchCategoriesStart | FetchCategoriesSuccess | FetchCategoriesFailed
 
 // Let's create action, STARTING Action there won't be no payload needed
-export const fetchCategoriesStart = () : FetchCategoriesStart =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesStart = withMatcher (() : FetchCategoriesStart =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START));
 
-export const fetchCategoriesSuccess = (categoriesArray: Category[]): FetchCategoriesSuccess =>
+export const fetchCategoriesSuccess = withMatcher((categoriesArray: Category[]): FetchCategoriesSuccess =>
   createAction(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
     categoriesArray
-  );
+  ));
 
-export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher ((error: Error): FetchCategoriesFailed =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error));
