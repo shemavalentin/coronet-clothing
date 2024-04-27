53d50b5c17e1a6764994f5d239a3a6939ff24a4e
Shema Valentin <shemavalentin@gmail.com>
2024-04-27 18:30:32 +0200

    Typed out Firebase utils and User action

---
+++ src/store/user/user.action.ts
@@ -0,0 +1,80 @@
+import { USER_ACTION_TYPES } from "./user.types";
+import { createAction, withMatcher, Action, ActionWithPayload } from "../../utils/reducer/reducer.utils";
+import { AdditionalInformation, UserData } from "../../utils/firebase/firebase.utils";
+
+
+// Creating action types for each of the function
+
+export type CheckUserSession = Action < USER_ACTION_TYPES.CHECK_USER_SESSION>;
+
+export type SetCurrentUser = ActionWithPayload < USER_ACTION_TYPES.SET_CURRENT_USER, UserData>;
+
+export type GoogleSignInStart = Action < USER_ACTION_TYPES.GOOGLE_SIGN_IN_START>;
+
+export type EmailSignInStart = ActionWithPayload < USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email: string, password: string }>;
+
+export type SignUpStart = ActionWithPayload < USER_ACTION_TYPES.SIGN_UP_START, { email: string, password: string, displayName: string }>
+
+export type SignInSuccess = ActionWithPayload < USER_ACTION_TYPES.SIGN_IN_SUCCESS, UserData>;
+
+export type SignInFailed = ActionWithPayload < USER_ACTION_TYPES.SIGN_IN_FAILED, Error>;
+
+export type SignUpSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user: UserData, additionalDetails: AdditionalInformation }>;
+
+export type SignUpFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_FAILED, Error>;
+
+export type SignOutStart = Action<USER_ACTION_TYPES.SIGN_OUT_START>;
+
+export type SignOutSuccess = Action<USER_ACTION_TYPES.SIGN_OUT_SUCCESS>;
+
+export type SignOutFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_OUT_FAILED, Error>;
+
+
+export const checkUserSession = withMatcher ((): CheckUserSession  =>
+  createAction(USER_ACTION_TYPES.CHECK_USER_SESSION));
+
+// Creating and returning back the action object
+export const setCurrentUser = withMatcher((user : UserData):SetCurrentUser =>
+  createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+
+
+export const googleSignInStart = withMatcher(() : GoogleSignInStart =>
+  createAction(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START));
+
+export const emailSignInStart = withMatcher ((email: string, password: string): EmailSignInStart =>
+  createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email, password }));
+
+export const signInSuccess = withMatcher ((user : UserData) : SignInSuccess =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user));
+
+export const signInFailed = withMatcher ((error: Error): SignInFailed =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error));
+
+// Creating actions for SignUp
+
+export const signUpStart = ((email: string, password: string, displayName: string): SignUpStart =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_START, {
+    email,
+    password,
+    displayName,
+  }));
+
+export const signUpSuccess = withMatcher((user : UserData, additionalDetails: AdditionalInformation): SignUpSuccess =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails }));
+
+export const signUpFailed = withMatcher ((error: Error): SignUpFailed =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error));
+
+// Creating actions for SignOut Form ( Here the signout doesn't need the payload)
+
+export const signOutStart = withMatcher ((): SignOutStart =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_START));
+
+// Here the success will be set to null
+export const signOutSuccess = withMatcher ((): SignOutSuccess =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS));
+
+// Fail alway need error as payload
+
+export const signOutFailed = withMatcher((error : Error): SignOutFailed =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error));
