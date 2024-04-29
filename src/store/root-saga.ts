dd77666ab8c2862fe44abb7d1d1ad6936f999038
Shema Valentin <shemavalentin@gmail.com>
2024-04-29 10:22:32 +0200

    Typed out root.saga

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,8 +1,8 @@
-import { all, call } from "redux-saga/effects";
+import { all, call } from "typed-redux-saga/macro";
 import { categoriesSaga } from "./categories/category.saga";
 import { UserSagas } from "./user/user.saga";
 
 // By using a generator function
 export function* rootSaga() {
-  yield all([call(categoriesSaga), call(UserSagas)]);
+  yield* all([call(categoriesSaga), call(UserSagas)]);
 }
