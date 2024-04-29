35bbb5e799325ea76f304cb6bd4e22c4d77aa33a
Shema Valentin <shemavalentin@gmail.com>
2024-04-29 12:59:06 +0200

    Typed sign-up-form.component

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, FormEvent, ChangeEvent } from "react";
+
+import { AuthError, AuthErrorCodes } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import FormInput from "../form-input/form-input.component";
 import "./sign-up-form.style.js";
@@ -27,7 +29,7 @@ const SignUpForm = () => {
     setFormFields(defaultFormFields);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (password !== confirmPassword) {
@@ -40,7 +42,7 @@ const SignUpForm = () => {
       dispacth(signUpStart(email, password, displayName));
       resetFormFields();
     } catch (error) {
-      if (error.code === "auth/email-already-in-use") {
+      if ((error as AuthError).code === AuthErrorCodes.EMAIL_EXISTS) {
         alert("Can not create user, email already in use");
       } else {
         console.log("User creation encountered an error", error);
@@ -49,7 +51,7 @@ const SignUpForm = () => {
   };
 
   // To be able to handle changes , we need to create a function to do so
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setFormFields({ ...formFields, [name]: value });
